75324c5a8c375bf3551690f49ff1215d01d49df7
codysilverglat <c.silverglat@gmail.com>
2020-07-02 22:36:14 -0500

    update sass variables

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Main from '../src/pages/main/main';
 import './index.css';
 
 
 function App() {
   return (
     <div className="App">
+      <Main></Main>
     </div>
   );
 }
---
+++ react-app/src/pages/main/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Main = () => {
+
+  return (
+    <>
+      <main>
+        <h1>Bundesliga</h1>
+      </main>
+    </>
+  )
+};
+
+export default Main;
