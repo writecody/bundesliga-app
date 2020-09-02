e74b76139a0fba4c4b6a6d1a5f26195d1081e04a
codysilverglat <c.silverglat@gmail.com>
2020-09-01 19:02:50 -0500

    add links in info section; style

--- react-app/src/components/global/teamsContainer/teamsContainer.js
+++ react-app/src/components/global/teamsContainer/teamsContainer.js
@@ -27,7 +27,12 @@ const TeamsContainer = () => {
         <div id="map" className="info-container__section">
           <MapboxGLMap />
         </div>
-
+        <div className="info-container__section">
+          <p>Learn more about the Bundesliga:</p>
+          <a className="info-container__section__link" href="https://www.bundesliga.com" target="_blank">Bundesliga Official Website</a>
+          <a className="info-container__section__link" href="https://en.wikipedia.org/wiki/Bundesliga" target="_blank">Bundesliga Wikipedia Article</a>
+          <a className="info-container__section__link" href="https://www.dfb.de/en/en-start/" target="_blank">Deutscher Fussball-Bund (German Football Association)</a>
+        </div>
       </div>
     </>
   )
