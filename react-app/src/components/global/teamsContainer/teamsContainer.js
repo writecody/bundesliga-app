56cea6fece8a3182d0ea36cf37549c76880ad5f7
codysilverglat <c.silverglat@gmail.com>
2020-09-02 17:57:07 -0500

    complete styling of initial version of app

--- react-app/src/components/global/teamsContainer/teamsContainer.js
+++ react-app/src/components/global/teamsContainer/teamsContainer.js
@@ -24,11 +24,11 @@ const TeamsContainer = () => {
           })}
       </div>
       <div className="info-container">
-        <div id="map" className="info-container__section">
+        <div id="map" className="info-container__map">
           <MapboxGLMap />
         </div>
-        <div className="info-container__section">
-          <p>Learn more about the Bundesliga:</p>
+        <div className="info-container__links">
+          <p>Learn more about the Bundesliga via the links below:</p>
           <a className="info-container__section__link" href="https://www.bundesliga.com" target="_blank">Bundesliga Official Website</a>
           <a className="info-container__section__link" href="https://en.wikipedia.org/wiki/Bundesliga" target="_blank">Bundesliga Wikipedia Article</a>
           <a className="info-container__section__link" href="https://www.dfb.de/en/en-start/" target="_blank">Deutscher Fussball-Bund (German Football Association)</a>
